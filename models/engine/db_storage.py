e179e443036be9b2f6a84dce2647a4411c2fe3f7
Osama0Fakroun <139139015+Osama0Fakroun@users.noreply.github.com>
2024-03-18 19:08:24 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@ from os import getenv
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 import models
-from models.base_model import Base
+from models.base_model
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
